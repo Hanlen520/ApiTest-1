263c1b5cab23db0ed66a4e25c97ff1f1ea821308
chenhongqing527 <laoqi1988_f1@126.com>
2018-03-14 11:31:38 +0800

    邮件显示报告内容

--- emailto.py
+++ emailto.py
@@ -3,6 +3,9 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+from email.mime.multipart import MIMEMultipart
+import datetime
+import time, os, sys,json,requests
 
 # 第三方 SMTP 服务
 mail_host="smtp.126.com"  #设置服务器
@@ -12,18 +15,25 @@ mail_pass="chx175246"   #密码
 sender = 'laoqi1988_f1@126.com'
 receivers = 'chenhongqing@yy.com'  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
-message = MIMEText('自动化测试结果'+'\n'+'总用例数量', 'plain', 'utf-8')
+message = MIMEMultipart()
+
 message['From'] = 'laoqi1988_f1@126.com'
 message['To'] = 'chenhongqing@yy.com'
 
-subject = '自动化测试报告'
+subject = 'API自动化测试报告'
 message['Subject'] = Header(subject, 'utf-8')
+TODAY = datetime.date.today()
+CURRENTDAY = TODAY.strftime('%Y-%m-%d')
+att = MIMEText(open('./Report.html', 'rb').read(), 'base64', 'utf-8')  # 设置附件的目录
+att['content-type'] = 'application/octet-stream'
+att['content-disposition'] = 'attachment;filename="%s result.html"'%CURRENTDAY  # 设置附件的名称
+message.attach(att)
 
-att1 = MIMEText('Report.html',_subtype="html",_charset='base64')
-att1["Content-Type"] = 'application/octet-stream'
-# 这里的filename可以任意写，写什么名字，邮件中显示什么名字
-# att1["Content-Disposition"] = 'attachment; filename="Report1.html"'
-message.attach(att1)
+body = MIMEText(open('Report.html', 'rb').read(), 'html', 'utf-8')  # 设置字符编码
+
+
+
+message.attach(body)
  
 
 smtpObj = smtplib.SMTP()
